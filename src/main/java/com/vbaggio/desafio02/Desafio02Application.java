31af03280c2a0247fae162050098416ac11a1351
vbaggio <vnc.baggio@gmail.com>
2024-09-20 17:04:07 -0300

    spring project initial setup

---
+++ src/main/java/com/vbaggio/desafio02/Desafio02Application.java
@@ -0,0 +1,13 @@
+package com.vbaggio.desafio02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio02Application.class, args);
+	}
+
+}
